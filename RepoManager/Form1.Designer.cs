725f80a2d738c045bfaaf9d8b6f8a891c75673d6
p6k0 <42266894+p6k0@users.noreply.github.com>
2019-02-20 21:45:57 +0300

    Initial commit

---
+++ RepoManager/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RepoManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FileNameLbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RepoNameTbx = new System.Windows.Forms.TextBox();
+            this.RepoVerTbx = new System.Windows.Forms.TextBox();
+            this.RepoDescrTbx = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Файл:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя репозитория:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Версия:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Описание:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FileNameLbl
+            // 
+            this.FileNameLbl.AutoEllipsis = true;
+            this.FileNameLbl.Location = new System.Drawing.Point(123, 9);
+            this.FileNameLbl.Name = "FileNameLbl";
+            this.FileNameLbl.Size = new System.Drawing.Size(132, 23);
+            this.FileNameLbl.TabIndex = 4;
+            this.FileNameLbl.Text = "label5";
+            this.FileNameLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RepoNameTbx
+            // 
+            this.RepoNameTbx.Location = new System.Drawing.Point(123, 35);
+            this.RepoNameTbx.Name = "RepoNameTbx";
+            this.RepoNameTbx.Size = new System.Drawing.Size(167, 20);
+            this.RepoNameTbx.TabIndex = 6;
+            // 
+            // RepoVerTbx
+            // 
+            this.RepoVerTbx.Location = new System.Drawing.Point(123, 57);
+            this.RepoVerTbx.Name = "RepoVerTbx";
+            this.RepoVerTbx.Size = new System.Drawing.Size(167, 20);
+            this.RepoVerTbx.TabIndex = 7;
+            // 
+            // RepoDescrTbx
+            // 
+            this.RepoDescrTbx.Location = new System.Drawing.Point(123, 80);
+            this.RepoDescrTbx.Multiline = true;
+            this.RepoDescrTbx.Name = "RepoDescrTbx";
+            this.RepoDescrTbx.Size = new System.Drawing.Size(167, 55);
+            this.RepoDescrTbx.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(55, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(62, 55);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Иконка";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 175);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RepoDescrTbx);
+            this.Controls.Add(this.RepoVerTbx);
+            this.Controls.Add(this.RepoNameTbx);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FileNameLbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Параметры репозитория";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label FileNameLbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RepoNameTbx;
+        private System.Windows.Forms.TextBox RepoVerTbx;
+        private System.Windows.Forms.TextBox RepoDescrTbx;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RepoManager/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RepoManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog f = new OpenFileDialog())
+            {
+                if (f.ShowDialog() != DialogResult.OK) return;
+                FileNameLbl.Text = f.FileName;
+                FileVersionInfo fv = FileVersionInfo.GetVersionInfo(f.FileName);
+                RepoNameTbx.Text = fv.ProductName;
+                RepoVerTbx.Text = fv.ProductVersion;
+                using (Icon ico = System.Drawing.Icon.ExtractAssociatedIcon(f.FileName))
+                    pictureBox1.Image = ico.ToBitmap();
+            }
+        }
+    }
+}
